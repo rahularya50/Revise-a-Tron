1b7249225249b03891f9f7cb73996d192074597b
rahularya50 <rahularya50@gmail.com>
2018-03-10 18:33:32 +0800

    Implemented textual modal display.

--- static/js/manage_modal.js
+++ static/js/manage_modal.js
@@ -6,6 +6,11 @@ $(document).ajaxComplete(function () {
     $(".fancy-table-row").click(function () {
         $("#editor").modal();
         var name = $(this).prop('name');
-
+        console.log("clicked");
+        $(this).find("td").each(function () {
+            console.log(this.dataset);
+            console.log("updating", this.dataset.column, this.dataset.value);
+            $("#" + this.dataset.column + "-indicator").html(this.dataset.value);
+        })
     })
 });
